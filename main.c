ece267f28529fe525a2bfe05d33b151a55e5f452
Jason Vander Woude <jsnvndrw@beaker-hn-1.cm.cluster>
2017-02-02 11:01:18 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+
+
+typedef struct {
+    //lookinto using char (1 byte) instead of int
+    int red;
+    int green;
+    int blue;
+} color;
+
+int main(int argc, char **argv) {
+    top_left_x = argv[1];
+    top_left_y = arvg[2];
+    bottom_left_x = argv[3];
+    bottom_left_y = arvg[4];
+    pix_width = argv[5];
+    pix_height = argv[6];
+
+    //spawn thread
+    //block on thread completion
+    //get saved string
+    //write string to file
+    //open file
+
+    return 0;
+}
+
+int compute_iterations(double real, double imag, int max_interations) {
+    //TODO
+}
+
+color get_color(int iterations) {
+    //TODO
+}
+
+complex pixel_to_complex(int pix_x, int pix_y) {
+    //TODO
+    //return the complex number associated with the pixel
+}
+
+
+
